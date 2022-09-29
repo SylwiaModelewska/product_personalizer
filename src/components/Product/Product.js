0aca918be7a07e18360a852f6131073e96518b89
Sylwia Modelewska <sylwiasupera@wp.pl>
2022-09-29 19:50:06 +0200

    Add useMemo

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,25 +2,26 @@ import styles from './Product.module.scss';
 import ProductOptions from '../ProductOptions/ProductOptions.js';
 import ProductImage from '../ProductImage/ProductImage.js';
 import PropTypes from 'prop-types';
-import { useState } from 'react';
+import { useState, useMemo } from 'react';
 
 const Product = props => {
 
   const [currentColor, setCurrentColor] = useState(props.colors[0]);
   const [currentSize, setCurrentSize] = useState(props.sizes[0].name);
 
-  const getPrice = () => {
+  const getPrice = useMemo (() => {
+    //console.log('getPrice');
     const element = props.sizes.find(({ name }) => name === currentSize);
     const additionalPrice = element.additionalPrice;
     return props.basePrice + additionalPrice;
-  }
+  }, [currentSize]);
 
   const prepareSummary = (e) => {
     e.preventDefault();
     console.log("Summary");
     console.log("=================");
     console.log("Name: ", props.title);
-    console.log("Price: ", getPrice());
+    console.log("Price: ", getPrice);
     console.log("Size: ", currentSize);
     console.log("Color: ", currentColor);
   }
@@ -31,7 +32,7 @@ const Product = props => {
       <div>
         <header>
           <h2 className={styles.name}>{props.title}</h2>
-          <span className={styles.price}>Price: {getPrice()}$</span>
+          <span className={styles.price}>Price: {getPrice}$</span>
         </header>
         <ProductOptions 
           product = {props}
